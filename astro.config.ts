e3f8dd65aae2aed2cfbb02e6dfe25e010e8e6ffe
Roger Filomeno <rpfilomeno@gmail.com>
2025-01-06 02:31:57 +0800

    deploy action

--- astro.config.ts
+++ astro.config.ts
@@ -23,7 +23,7 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
 
 export default defineConfig({
   output: 'static',
-  site: 'https://rpfilomeno.github.io',
+  site: 'https://rogverse.fyi',
   base: '/rogverse.fyi',
 
   integrations: [
