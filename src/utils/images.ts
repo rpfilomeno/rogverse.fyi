519e0390d9ab52d2c21ab1c7a16f290a967944c9
Roger Filomeno <rpfilomeno@gmail.com>
2025-01-06 01:52:23 +0800

    use never

--- src/utils/images.ts
+++ src/utils/images.ts
@@ -71,7 +71,7 @@ export const adaptOpenGraphImages = async (
           };
         }
 
-        let _image: any;
+        let _image: never;
 
         if (
           typeof resolvedImage === 'string' &&
